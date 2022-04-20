055ccc2d1bd91cc2eeb6e81988b46ae329059bcb
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-04-20 12:27:42 +0200

    creating build error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const justAlog=()=>{
+const justAlog()=>{
     
     console.log('justAlog from index.js. Richard if you see this message, the build went well'); 
     console.log('Well done. This is another long that you should be able to see somewhere in the console');
