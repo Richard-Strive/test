1969830ca1ada16b08008652d6f06178f5027b3a
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-04-20 11:44:27 +0200

    let's goooo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const justAlog=()=>{
+    
     console.log('justAlog from index.js. Richard if you see this message, the build went well'); 
+    console.log('Well done. This is another long that you shold be able to see somewhere in the console');
 }
 
 justAlog()
